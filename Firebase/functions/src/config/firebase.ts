f85d3153ff31a1b14ee2102df9e317999f809d70
Hydeo <p.garciapelayo@gmail.com>
2021-03-21 12:54:31 +0000

    [Linting]

--- Firebase/functions/src/config/firebase.ts
+++ Firebase/functions/src/config/firebase.ts
@@ -1,11 +1,13 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
-// We retrieve the config value set previously in the firebase project vars with 
+// We retrieve the config value set previously in the firebase project vars with
 // firebase functions:config:set
 admin.initializeApp({
   credential: admin.credential.cert({
-    privateKey: functions.config().private.key.replace(/\\n/g, "\n"), // We replace the the Private key break line signs to evoid a "Invalid PEM format" error
+    /* We replace the the Private key break line signs
+    to evoid a "Invalid PEM format" error */
+    privateKey: functions.config().private.key.replace(/\\n/g, "\n"),
     projectId: functions.config().project.id,
     clientEmail: functions.config().client.email,
   }),
