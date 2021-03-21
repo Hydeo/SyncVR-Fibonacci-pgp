754ecd648ccbae1549cb5a63a774a1a8d5323f5b
Hydeo <p.garciapelayo@gmail.com>
2021-03-21 22:56:35 +0000

    Deploy

--- Firebase/functions/src/index.ts
+++ Firebase/functions/src/index.ts
@@ -1,8 +1,12 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 import {addFiboEntry, getAllFiboEntries} from "./fiboController";
+const cors = require("cors");
 
 const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({origin: true}));
 
 app.post("/", addFiboEntry);
 app.get("/", getAllFiboEntries);
